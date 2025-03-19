1569829eca5370953bef56b35f4d5d15a93ab2f6
user.email <ssuvorov096@gmail.com>
2025-03-19 19:08:18 +0300

    version 1.1.3

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,111 @@
 #include <Arduino.h>
 
+#define WiFi_SSID "infinix" // имя сети
+#define WiFi_PASS "123456789" // пароль сети
+
+ // иниализация библиотеки датчика темпиратуры
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+DHT dht(20, DHT11);
+
+// иниализация библииотеки дисплея
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 
-#define CS 10
-#define RES 8
-#define DC 9
+#define CS 5
+#define RES 16
+#define DC 17
 
 Adafruit_ST7735 tft = Adafruit_ST7735(CS, DC, RES);
 
-void setup(){
+// иниализация библиотеки для сайта
+#include <WiFi.h>
+#include<GyverDBFile.h>
+#include<LittleFS.h>
+GyverDBFile db(&LittleFS, "/data.db");
 
-  tft.initR(INITR_18BLACKTAB); // иниализация дисплея
-  tft.setRotation(4);
-  tft.fillScreen(ST7735_BLACK);
-/*
+#include<SettingsGyver.h>
+SettingsGyver sett("My settings", &db);
+
+
+#define buzz // пишалка
+#define led_1 // светодиод 1
+#define led_2 // светодиод 2
+
+#define gas // датчик газа
+#define fire // датчик огня
+
+enum kk : size_t {
+
+};
+
+void build(sets::Builder& b){
+   
+
+}
+
+ void setup(){
+
+   Serial.begin(115200);
+   Serial.println("");
+   
+    dht.begin();
+    tft.initR(INITR_18BLACKTAB); // иниализация дисплея
+    tft.setRotation(4);
+
+    tft.fillScreen(ST7735_BLACK);
+
+    WiFi.begin(WiFi_SSID, WiFi_PASS);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("Вы подключились сети WiFi!!! ");
+    Serial.println(WiFi.localIP());
+    
+
+    sett.begin();
+    sett.onBuild(build);
+
+   // экран загрузки
+   tft.setCursor(20, 20);
+   tft.setTextSize(3);
+   tft.setTextColor(ST7735_GREEN);
+   tft.print("ESP32");
+
+   tft.setCursor(23, 70);
+   tft.setTextSize(2);
+   tft.print("Loading");
+
+   tft.drawFastHLine(10, 90, 105, ST77XX_BLUE);
+   tft.drawFastHLine(10, 120, 105, ST77XX_BLUE);
+   tft.drawFastVLine(10, 90, 30, ST77XX_BLUE);
+   tft.drawFastVLine(115, 90, 30, ST77XX_BLUE);
+   
+   // цикл для эффекта закрузки 
+   for (int i = 10; i <= 105; i++){
+      tft.fillRect(10, 90, i, 30, ST7735_BLUE);
+      delay(100);
+   }
+
+   tft.setCursor(35, 140);
+   tft.setTextSize(1);
+   tft.setTextColor(ST7735_GREEN);
+   tft.print("by Lino-e");
+   delay(1000);
+
+   
+    }
+
+ void loop(){
+ }
+
+void loading(){
   tft.setCursor(20, 20);
   tft.setTextSize(3);
   tft.setTextColor(ST7735_GREEN);
@@ -43,12 +133,10 @@ void setup(){
   delay(1000);
 
   tft.fillScreen(ST7735_BLACK);
-*/
 }
 
-void loop(){
-  /*
-tft.drawFastHLine(0, 20, 128, ST7735_ORANGE);
+void home(){
+  tft.drawFastHLine(0, 20, 128, ST7735_ORANGE);
 tft.drawFastHLine(0, 21, 128, ST7735_ORANGE);
 tft.drawLine(35, 0, 50, 10, ST7735_ORANGE);
 tft.drawLine(36, 0, 51, 10, ST7735_ORANGE);
@@ -132,8 +220,10 @@ tft.drawLine(94, 146, 89, 156, ST7735_ORANGE);
   tft.setTextSize(2);
   tft.setCursor(15, 140);
   tft.print("23 ppm");
-*/
-tft.setTextColor(ST7735_GREEN);
+}
+
+void indicators(){
+  tft.setTextColor(ST7735_GREEN);
 tft.setTextSize(2);
 tft.setCursor(5, 0);
 tft.print("Indicators");
@@ -190,3 +280,5 @@ tft.drawCircle(95, 130, 24, ST7735_ORANGE);
 }
 
   
+
+ 
